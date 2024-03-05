4707d2cd98374a82e18b3b85bfc7a36fb38c250f
agildo <agildo.junior@simpleshotel.com.br>
2024-03-05 19:32:10 -0300

    config authorize swagger

--- APIFormsX-Wing/Program.cs
+++ APIFormsX-Wing/Program.cs
@@ -1,3 +1,4 @@
+using APIFormsX_Wing;
 using APIFormsX_Wing.Data;
 using APIFormsX_Wing.Models;
 using APIFormsX_Wing.Repositorys;
@@ -11,7 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddInfrastructureSwagger();
+//builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<SystemDBContext>(
     options => options.UseNpgsql(builder.Configuration.GetConnectionString("Database"))
